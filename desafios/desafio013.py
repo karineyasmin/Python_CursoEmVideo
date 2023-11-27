5803e023c9b995b08e2a0b0724a943990a945295
Karine Yasmin <130607138+karineyasmin@users.noreply.github.com>
2023-11-26 23:17:04 -0300

    Atualizar o desafio013.py

--- desafios/desafio013.py
+++ desafios/desafio013.py
@@ -1,7 +1,11 @@
-#desafio013 algortimo que leia o salario de um funcionario e mostr seu novo salario com 15% de aumento
+#desafio013 - Algortimo que lê o salário de um funcionário e mostra seu novo salário com 15% de aumento.
+
+#CODIGO:
 
 salario = float(input('Digite o salário: '))
+
 aumento = (salario * 15) / 100
+
 salario_final = salario + aumento 
 
 print(f'\nCom o aumento de 15%, o novo salário será: {salario_final}')
