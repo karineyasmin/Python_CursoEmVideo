83d6fa3bf83360d1c4a740c02e2ea85bccf0f361
Karine Yasmin <130607138+karineyasmin@users.noreply.github.com>
2023-11-26 23:20:00 -0300

    Atualizar o desafio015.py

--- desafios/desafio015.py
+++ desafios/desafio015.py
@@ -1,13 +1,20 @@
+#desafio015
+
 '''
-programa que pergunte a quantidade de km percorridos por um carro alugado e a qntd de dias 
-pelo de aluguel. calcule o preço a pagar sabendo que o carro custa 60/dia e R$ 0,15 por km rodado 
+Crie um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias de aluguel. 
+Calcule o preço a pagar sabendo que o carro custa 60/dia e R$ 0,15 por km rodado.
 '''
+
+#CODIGO:
 
 diaria_preço = 60.00
 kmRodado_preço = 0.15
+
 print('-' * 40)
+
 diaria = int(input('Quantos dias o veículo foi utilizado? '))
 kmRodados = float(input('Quantos KM foram percorridos? '))
+
 diariaTotal = diaria_preço * diaria
 kmTotal = kmRodado_preço * kmRodados
 totalValor = diariaTotal + kmTotal
