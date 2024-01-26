658baa148f041410b8bf24802c5534ddbdf17a29
Karine <karine.yasmin@outlook.com>
2024-01-26 18:54:15 -0300

    add

--- Mundo 2/desafio046.py
+++ Mundo 2/desafio046.py
@@ -6,6 +6,8 @@ Enunciado:
 Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício.
 Indo de 10 até 0, com uma pausa de 1 segundo entre eles.
 '''
+#código
+
 from time import sleep
 from emoji import emojize
 
